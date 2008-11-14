d791f0997a24c4e42a08f08be2fbd87e77491fef
Bradley.Kite <Bradley.Kite@349629ee-b03d-11dd-8830-69e4bb380a4a>
2008-11-14 23:05:55 +0000

    Made a start at porting the mapnik map renderer. I've mostly been going through header files to get the structures/classes and porting those across. Far from complete.

---
+++ src/org/andnav/osm/renderer/mapnik/MapnikLinePatternSymbolizer.java
@@ -0,0 +1,19 @@
+package org.andnav.osm.renderer.mapnik;
+
+// Original from include/mapnik/line_pattern_symbolizer.hpp
+
+public class MapnikLinePatternSymbolizer {
+
+	
+	
+	/*
+    struct MAPNIK_DECL line_pattern_symbolizer :
+        public symbolizer_with_image
+    {
+        line_pattern_symbolizer(std::string const& file,
+                                std::string const& type,
+                                unsigned width,unsigned height);
+        line_pattern_symbolizer(line_pattern_symbolizer const& rhs);
+    }; */
+
+}
---
+++ src/org/andnav/osm/renderer/mapnik/MapnikRule.java
@@ -0,0 +1,10 @@
+package org.andnav.osm.renderer.mapnik;
+
+// Original from include/mapnik/rule.hpp
+
+public class MapnikRule {
+
+	
+
+	
+}
