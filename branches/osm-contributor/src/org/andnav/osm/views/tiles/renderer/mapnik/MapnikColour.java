d0c6a670ee2c9cbd4808c362b8375cc2a69f8525
Bradley.Kite <Bradley.Kite@349629ee-b03d-11dd-8830-69e4bb380a4a>
2008-11-21 11:38:08 +0000

    Changed encoding to match that of android.graphics.Color

--- branches/osm-contributor/src/org/andnav/osm/views/tiles/renderer/mapnik/MapnikColour.java
+++ branches/osm-contributor/src/org/andnav/osm/views/tiles/renderer/mapnik/MapnikColour.java
@@ -5,71 +5,71 @@ package org.andnav.osm.views.tiles.renderer.mapnik;
 
 public class MapnikColour {
 
-	private int m_abgr;
+	private int m_argb;
 
 	public MapnikColour() {
-		m_abgr = 0xffffffff;
+		m_argb = 0xffffffff;
 	}
 
 	public MapnikColour(int red, int green, int blue, int alpha) {
-		m_abgr = ((alpha & 0xff) << 24 | (blue & 0xff) << 16 | (green & 0xff) << 8 | (red & 0xff));
+		m_argb = ((alpha & 0xff) << 24 | (red & 0xff) << 16 | (green & 0xff) << 8 | (blue & 0xff));
 	}
 	
 	public MapnikColour(int red, int green, int blue) {
 		int alpha = 0xff;
-		m_abgr = ((alpha & 0xff) << 24 | (blue & 0xff) << 16 | (green & 0xff) << 8 | (red & 0xff));
+		m_argb = ((alpha & 0xff) << 24 | (red & 0xff) << 16 | (green & 0xff) << 8 | (blue & 0xff));
 	}
 
 	public MapnikColour(int abgr) {
-		m_abgr = abgr;
+		m_argb = abgr;
 	}
 
 	public MapnikColour(MapnikColour c) {
-		m_abgr = c.m_abgr;
+		m_argb = c.m_argb;
+	}
+	
+	public int getAlpha() {
+		return (m_argb >> 24) & 0xff;
 	}
 
 	public int getRed() {
-		return m_abgr & 0xff;
+		return (m_argb) >> 16 & 0xff;
 	}
 
 	public int getGreen() {
-		return (m_abgr >> 8) & 0xff;
+		return (m_argb >> 8) & 0xff;
 	}
 
 	public int getBlue() {
-		return (m_abgr) >> 16 & 0xff;
-	}
-
-	public int getAlpha() {
-		return (m_abgr >> 24) & 0xff;
-	}
-
-	public void setRed(int r) {
-		m_abgr = (m_abgr & 0xffffff00) | (r & 0xff);
-	}
-
-	public void setGreen(int g) {
-		m_abgr = (m_abgr & 0xffff00ff) | ((g & 0xff) << 8);
-	}
-
-	public void setBlue(int b) {
-		m_abgr = (m_abgr & 0xff00ffff) | ((b & 0xff) << 16);
+		return m_argb & 0xff;
 	}
 
 	public void setAlpha(int a) {
-		m_abgr = (m_abgr & 0x00ffffff | (a & 0xff) << 24);
+		m_argb = (m_argb & 0x00ffffff | (a & 0xff) << 24);
+	}
+	
+	public void setRed(int b) {
+		m_argb = (m_argb & 0xff00ffff) | ((b & 0xff) << 16);
 	}
 
-	public int getRGBA() {
-		return m_abgr;
+	public void setGreen(int g) {
+		m_argb = (m_argb & 0xffff00ff) | ((g & 0xff) << 8);
+	}
+	
+	public void setBlue(int r) {
+		m_argb = (m_argb & 0xffffff00) | (r & 0xff);
 	}
 
-	public void setBGR(int bgr) {
-		m_abgr = (m_abgr & 0xff000000) | (bgr & 0xffffff);
+	public int getARGB() {
+		return m_argb;
+	}
+
+	public void setBGR(int rgb) {
+		m_argb = (m_argb & 0xff000000) | (rgb & 0xffffff);
 	}
 
 	public boolean equals(final MapnikColour c) {
-		if (m_abgr == c.m_abgr)
+		if (m_argb == c.m_argb)
 			return true;
 		return false;
 	}
